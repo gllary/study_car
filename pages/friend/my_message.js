26684610f93fc6f1c54d8dbd71c95d9638579f44
Gyx <1549624673@whut.edu.cn>
2018-10-27 09:29:05 +0800

    Update my_message.js

--- pages/friend/my_message.js
+++ pages/friend/my_message.js
@@ -174,35 +174,49 @@ Page({
         app.globalData.flag = res.data
         console.log("成功")
         console.log(that.data.circleid)
-      /*  var uploadImgCount = 0;
+      
         for (var i = 0, h = that.data.imgs.length; i <= h; i++) {
-          wx.uploadFile({
-            url: 'https://lieyan-1257158697.cos.ap-shanghai.myqcloud.com/circle/',
-            filePath: that.data.imgs[i],
-            name: that.data.circleid + 'image_' + i + '.png',
-            success: function (res) {
-              wx.showToast({
-                title: '成功上传',
-
-              })
-              wx.switchTab({
+  /*** */
+wx.uploadFile({
+url: '/', //开发者服务器的 url
+filePath: that.data.imgs[0], // 要上传文件资源的路径 String类型！！！
+name: 'file', // 文件对应的 key ,(后台接口规定的关于图片的请求参数)
+header: {
+'content-type': 'multipart/form-data'
+},
+// 设置请求的 header
+formData: {
+'key': "circle/" + that.data.circleid + "image_" +i+ ".png"
+},
+// HTTP 请求中其他额外的参数
+success: function (res) {
+wx.showToast({
+title: '成功上传！',
+})
+console.log("shang")
+  wx.request({
+          url: '',
+      method: 'POST',
+      header: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: {
+        openid: that.data.openid,
+        mess: that.data.content
+      },
+     success: function (res) {
+          wx.switchTab({
                 url: '../friend/friend',
-              })
+              })    
+     }
+  })
+},
+fail: function (res) {
+}
+})
+//**** */
 
-            }
-          })
-        }*/
- 
-        wx.showToast({
-          title: '成功上传',
-        })
       }
     })
-     //传图片
-    that.uploadimg({
-      url: '',
-      path: that.data.imgs
-    })
+
 
     wx.switchTab({
       url: '../friend/friend',
@@ -210,42 +224,6 @@ Page({
 
    
   },
-  uploadimg:function(data){
-    var that = this,
-    i=data.i ? data.i : 0,//当前上传的哪张图片
-    success=data.success ? data.success : 0,//上传成功的个数
-    fail=data.fail ? data.fail : 0;//上传失败的个数
-    wx.uploadFile({
-      url: data.url,
-      filePath: data.path[i],
-      name: that.data.circleid + 'image_' + i + '.png',//这里根据自己的实际情况改
-      formData: null,//这里是上传图片时一起上传的数据
-      success: (resp) => {
-        success++;//图片上传成功，图片上传成功的变量+1
-        console.log(resp)
-        console.log(i);
-        //这里可能有BUG，失败也会执行这里,所以这里应该是后台返回过来的状态码为成功时，这里的success才+1
-      },
-      fail: (res) => {
-        fail++;//图片上传失败，图片上传失败的变量+1
-        console.log('fail:' + i + "fail:" + fail);
-      },
-      complete: () => {
-        console.log(i);
-        i++;//这个图片执行完上传后，开始上传下一张
-        if (i == data.path.length) {   //当图片传完时，停止调用          
-          console.log('执行完毕');
-          console.log('成功：' + success + " 失败：" + fail);
-        } else {//若图片还没有传完，则继续调用函数
-          console.log(i);
-          data.i = i;
-          data.success = success;
-          data.fail = fail;
-          that.uploadimg(data);
-        }
 
-      }
-    });
-  },
 
 })
